804bbf07bb9cc174b779451199339ddc6068d4fd
Lin2808 <linjaviers@hotmail.com>
2022-09-09 00:48:20 -0500

    Obtiene el json con las estadísticas del pokémon

--- src/app/pokemon/pokemon.component.ts
+++ src/app/pokemon/pokemon.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PokemonService } from '../services/pokemon.service';
 
 @Component({
   selector: 'app-pokemon',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pokemon.component.css']
 })
 export class PokemonComponent implements OnInit {
-
-  constructor() { }
+  name:string = ''
+  constructor(private pokemonService : PokemonService) { }
 
   ngOnInit(): void {
+  }
+  search()
+  {
+    this.pokemonService.getPokemon(this.name).subscribe(data =>{console.log(data)});
   }
 
 }
